59afc40a112c3e0fcb23529de624c1fad1918887
29244 <29244@ma-web.nl>
2021-04-06 16:11:51 +0200

    23456

--- VRSHIT/Assets/Ardity/Scripts/Samples/SampleUserPolling_ReadWrite.cs
+++ VRSHIT/Assets/Ardity/Scripts/Samples/SampleUserPolling_ReadWrite.cs
@@ -85,11 +85,6 @@ public class SampleUserPolling_ReadWrite : MonoBehaviour
             return;
 
         // Check if the message is plain data or a connect/disconnect event.
-        if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
-            Debug.Log("Connection established");
-        else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
-            Debug.Log("Connection attempt failed or disconnection detected");
-        else
-            Debug.Log("Message arrived: " + message);
+        
     }
 }
