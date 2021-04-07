368a6ed1c816bace810ea81957fc4617f9d0c3ab
Bjornraaf <daysforyou1120@gmail.com>
2021-04-07 20:52:11 +0200

    Light 1
    e

---
+++ VRSHIT/Assets/Scripts/Light1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Light1 : MonoBehaviour
+{
+    public SerialController serialController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Sending A");
+        serialController.SendSerialMessage("A");
+    }
+
+    public void turnOff()
+    {
+        Debug.Log("Sending F");
+        serialController.SendSerialMessage("F");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
