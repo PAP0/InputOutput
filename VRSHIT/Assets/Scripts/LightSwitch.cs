6b1a1a7c36b80d3e7e47fd87bf329bb072b89bfb
29244 <29244@ma-web.nl>
2021-04-08 00:51:40 +0200

    Revert "Merge branch 'main' of https://github.com/D-RaMedia/InputOutput into main"
    This reverts commit 210fceb714524096c929a1db229b37dae8e39d98, reversing
    changes made to bf2e462210cab59b3fc1b711ee1e6b0286b5299b.

--- VRSHIT/Assets/Scripts/LightSwitch.cs
+++ VRSHIT/Assets/Scripts/LightSwitch.cs
@@ -4,81 +4,46 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
 {
-public GameObject spotlight1;
-public GameObject spotlight2;
-public GameObject spotlight3;
-public GameObject bwokenWire;
-public GameObject winWindow;
-private int E;
-public SerialController serialController;
-
-void Start()
-{
-    serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
-
-    Debug.Log("Sending A");
-    serialController.SendSerialMessage("A");
-
-    Debug.Log("Sending B");
-    serialController.SendSerialMessage("B");
-
-    Debug.Log("Sending C");
-    serialController.SendSerialMessage("C");
-
-}
-
-
-
-void Update()
-{
-
-    if (bwokenWire.activeSelf)
+    public GameObject spotlight1;
+    public GameObject spotlight2;
+    public GameObject spotlight3;
+    public GameObject bwokenWire;
+    public GameObject winWindow;
+    private int E;
+    public SerialController serialController;
+    void Start()
     {
-        Debug.Log("Sending F");
-        serialController.SendSerialMessage("F");
-        spotlight1.SetActive(true);
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+
+        Debug.Log("Sending A");
+        serialController.SendSerialMessage("A");
+
+        Debug.Log("Sending B");
+        serialController.SendSerialMessage("B");
+
+        Debug.Log("Sending C");
+        serialController.SendSerialMessage("C");
+
     }
 
-    if (E >= 3)
+
+
+    void Update()
     {
-        E = 3;
-    }
-
-    if (spotlight1.activeSelf)
-    {
-        winWindow.SetActive(true);
-    }
-
-        if (Input.GetKeyDown(KeyCode.A))
+        if (bwokenWire.activeSelf)
         {
-            Debug.Log("Sending A");
-            serialController.SendSerialMessage("A");
+            spotlight1.SetActive(true);
+            E++;
         }
 
-        if (Input.GetKeyDown(KeyCode.B))
+        if (E >= 3)
         {
-            Debug.Log("Sending B");
-            serialController.SendSerialMessage("B");
+            E = 3;
         }
-        if (Input.GetKeyDown(KeyCode.C))
+
+        if (E == 1)
         {
-            Debug.Log("Sending C");
-            serialController.SendSerialMessage("C");
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Debug.Log("Sending F");
-            serialController.SendSerialMessage("F");
-        }
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            Debug.Log("Sending G");
-            serialController.SendSerialMessage("G");
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            Debug.Log("Sending H");
-            serialController.SendSerialMessage("H");
+            winWindow.SetActive(true);
         }
         //---------------------------------------------------------------------
         // Receive data
@@ -86,17 +51,16 @@ void Update()
 
         string message = serialController.ReadSerialMessage();
 
-    if (message == null)
-        return;
+        if (message == null)
+            return;
 
-    // Check if the message is plain data or a connect/disconnect event.
-    if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
-        Debug.Log("Connection established");
-    else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
-        Debug.Log("Connection attempt failed or disconnection detected");
-    else
-        Debug.Log("Message arrived: " + message);
+        // Check if the message is plain data or a connect/disconnect event.
+        if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+            Debug.Log("Connection established");
+        else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+            Debug.Log("Connection attempt failed or disconnection detected");
+        else
+            Debug.Log("Message arrived: " + message);
     }
 
 }
-
